517b95e1d40abe150c95aa90fcdde1cd9697c1bb
Jack <jack@top.com>
2020-02-19 18:15:53 +0800

    xx

--- VideoProject/venv/Project/app.py
+++ VideoProject/venv/Project/app.py
@@ -1,13 +1,17 @@
 from flask import Flask,render_template,request,url_for,redirect,make_response,send_file, send_from_directory,jsonify
 from os import *
 from Project import VideoModu
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+Bootstrap(app)
 
 #启动测试
 @app.route('/')
 def hello():
-    return render_template('1.html',name='哈哈')
+
+    return render_template('index.html',name='哈哈')
 
 
 #关键字搜索
@@ -32,7 +36,14 @@ def timeperMethod():
 
     pass
 
+##############################错误或者服务器错误##################################################################################################
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 def ResponseMethod(code,msg,records):
     data = {
@@ -41,6 +52,7 @@ def ResponseMethod(code,msg,records):
         'records':records,
     }
     return data
+################################################################################################################################
 
 
 if __name__ == '__main__':
